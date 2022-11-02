b212ffce619f74bec0338678d9de680ee821dbf4
Hyolim <hyolim93@gmail.com>
2022-11-02 12:22:49 +0900

    early model
    early model

--- democodes_chik.R
+++ democodes_chik.R
@@ -9,17 +9,10 @@ require(MCMCvis)
 library(cowplot)
 library(Rsero)
 
-
-library(readxl)
+##Read file
 chik_systematic_review_v1 <- read_excel("~/Library/CloudStorage/OneDrive-LondonSchoolofHygieneandTropicalMedicine/CHIK/1.Aim1/all_countries/chik_systematic_review_v1.xlsx", 
                                         sheet = "prac")
-View(chik_systematic_review_v1)
-
-library(readxl)
-chik_systematic_review_v1 <- read_excel("C:/Users/Hyolim/OneDrive - London School of Hygiene and Tropical Medicine/CHIK/1.Aim1/all_countries/chik_systematic_review_v1.xlsx", 
-                                        sheet = "prac")
-View(chik_systematic_review_v1)
-
+#Rename data
 df_chik = chik_systematic_review_v1 
 
 df_chik[,c("mid","lo","hi")] = binom.confint(df_chik$N.pos, df_chik$N, method="exact")[,c("mean","lower","upper")]
@@ -27,11 +20,12 @@ df_chik[,c("mid","lo","hi")] = binom.confint(df_chik$N.pos, df_chik$N, method="e
 df_chik <-  df_chik %>% 
   dplyr::mutate(agemid = (age_min+age_max)/2)
 
+#sub-setting datafiles by study
 datS1 <- df_chik[df_chik$study == "1",]
 datS2 <- df_chik[df_chik$study == "2",]
 datS3 <- df_chik[df_chik$study == "3",]
 
-
+# Define model
 jcode <- "model{ 
 	for (i in 1:3){
     n.pos[i] ~ dbinom(seropos_est[i],N[i]) #fit to binomial data
@@ -54,6 +48,7 @@ jcode <- "model{
 
 #vector 
 paramVector <- c("lambda_1", "lambda_2", "lambda_3")
+
 # Run model
 mcmc.length=50000
 jdat = list(n.pos= df_chik$N.pos,
@@ -91,40 +86,3 @@ for(i in 1:length(paramEstimates)){
 }
 
 paramDat = data.frame(paramVector,varOutput)
-
-# create data  for plots 
-ager=0:85
-numSamples = 1000
-foiVector = paramVector[1:3]
-
-for (ii in 1:length(foiVector)) {
-  outDf <- matrix(NA,nrow=numSamples, ncol = length(ager))
-  foiStudy <- foiVector[ii]
-  for (kk in 1:numSamples) {
-    randomNumber <- floor(runif(1, min = 1, max = nrow(mcmcMatrix)))
-    lambdaSample <- mcmcMatrix[randomNumber,foiStudy]
-    newRow <- 1-exp(-lambdaSample*ager)
-    outDf[kk,] <- newRow
-  }
-  quantileMatrix <- matrix(NA,nrow=ncol(outDf), ncol = 3)
-  for(jj in 1:ncol(outDf)){
-    quantiles <- outDf[,jj] %>% quantile(probs=c(.5,.025,.975))
-    quantileMatrix[jj,] <- quantile
-  }
-  df_upperLower = data.frame(
-    midpoint = ager,
-    upper = quantileMatrix[,3],
-    lower = quantileMatrix[,2]
-  )
-  
-  assign(paste0("df_", foiVector[ii]), df_upperLower)
-}
-
-
-ggplot(df_mod_chik, aes(x= agemid, y= mid, ymin=lo, ymax=hi)) +
-  geom_ribbon(alpha=0.2)+
-  geom_line()+
-  geom_point(data=df_chik)+
-  geom_linerange(data=df_chik) +
-  xlab("Age (years)") + ylab("seropositivity") +
-  scale_x_continuous(breaks=seq(0,100,by=5))
